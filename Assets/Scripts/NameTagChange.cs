5d96437a5e99444c723a7d2c89cbb21e6cd0cafa
Raphael Kriegl <raphael-kriegl@web.de>
2020-11-06 00:29:37 +0100

    yarn nametag command done

---
+++ Assets/Scripts/NameTagChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn;
+using Yarn.Unity;
+
+public class NameTagChange : MonoBehaviour
+{
+    public DialogueRunner dialogueRunner;
+    public TMPro.TextMeshProUGUI nameTag;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        dialogueRunner.AddCommandHandler("nametag", ChangeNameTag);
+    }
+
+    public void ChangeNameTag(string[] parameters)
+    {
+        nameTag.text = parameters[0];
+    }
+}
