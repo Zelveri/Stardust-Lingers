e42952de2a72165893203e165567bdb11d0baf04
Raphael <>
2020-11-09 17:18:06 +0100

    start save state implementation

---
+++ Assets/Scripts/DialogueTracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogueTracker : MonoBehaviour
+{
+
+    public DialogueRunner dialogueRunner;
+    List<string> lines;
+    string currentNode;
+    string prevNode;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        lines = new List<string>();
+    }
+
+    public void AddLineToTracker(string line)
+    {
+        lines.Add(line);
+    }
+
+    public void UpdateCurrentNode(string node)
+    {
+        currentNode = node;
+    }
+
+    public void UpdateFinishedNode(string node)
+    {
+        prevNode = node;
+    }
+}
---
+++ Assets/Scripts/SaveState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveState
+{
+    string currentNode;
+    string prevNode;
+    string[] lines;
+    string[] choices;
+
+    SaveState(string curNode = "Start", string prevNode = "", string[] lines = null, string[] choices= null)
+    {
+        this.currentNode = curNode;
+        this.prevNode = prevNode;
+        this.lines = lines;
+        this.choices = choices;
+    }
+}
