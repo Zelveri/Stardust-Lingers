acccfb1207f147d6c06062b2911f0fc915e3b468
Raphael <>
2020-11-11 18:06:02 +0100

    Menu scene functionality
    Panel selection working, nothing else

---
+++ Assets/Scripts/Menu/MenuBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFileClick()
+    {
+        GameObject fileUI = gameObject.transform.Find("SaveUIPanel").gameObject;
+        GameObject settingsUI = gameObject.transform.Find("SettingsUIPanel").gameObject;
+        settingsUI.SetActive(false);
+        fileUI.SetActive(true);
+    }
+
+    public void OnReturnClick()
+    {
+
+    }
+
+    public void OnGearClick()
+    {
+        GameObject fileUI = gameObject.transform.Find("SaveUIPanel").gameObject;
+        GameObject settingsUI = gameObject.transform.Find("SettingsUIPanel").gameObject;
+        fileUI.SetActive(false);
+        settingsUI.SetActive(true);
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
