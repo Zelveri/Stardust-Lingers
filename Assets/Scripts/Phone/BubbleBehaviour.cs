daf4880a0bab3c8d10475e06b4aff5c9dd629ba5
Raphael <>
2020-11-14 22:33:02 +0100

    phone scene and autosizing speech bubbles
    also some dialogue runner settings

---
+++ Assets/Scripts/Phone/BubbleBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class BubbleBehaviour : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI textField;
+    public Image top;
+    public Image middle;
+    public Image bottom;
+
+    string text = "";
+    float oldHeight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateText("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(TestText());
+        }
+    }
+
+    IEnumerator TestText()
+    {
+        while (true)
+        {
+            text += "a\n";
+            UpdateText(text);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void UpdateText(string text)
+    {
+        textField.text = text;
+        AdjustBubbleSize();
+    }
+
+    public float GetHeight()
+    {
+        return top.rectTransform.rect.height + middle.rectTransform.rect.height + bottom.rectTransform.rect.height;
+    }
+
+    void AdjustBubbleSize()
+    {
+        var movVect = new Vector3(0, textField.rectTransform.rect.height - middle.rectTransform.rect.height, 0);
+        middle.rectTransform.sizeDelta = new Vector2(middle.rectTransform.rect.width, textField.rectTransform.rect.height);
+    
+        top.rectTransform.Translate(movVect);
+    }
+
+    public void MoveUp(float height)
+    {
+        var movVect = new Vector3(0, height,0);
+        top.rectTransform.Translate(movVect);
+        middle.rectTransform.Translate(movVect);
+        bottom.rectTransform.Translate(movVect);
+    }
+}
---
+++ Assets/Scripts/Phone/LineUpdateHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LineUpdateHandler : MonoBehaviour
+{
+    public List<BubbleBehaviour> meBubbles;
+    public BubbleBehaviour[] themBubbles;
+
+    public string text
+    {
+        set { UpdateLine(value); }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLine(string text)
+    {
+        meBubbles.LastOrDefault().UpdateText(text);
+    }
+
+    public void DialogueStart()
+    {
+
+    }
+
+    public void LineStart()
+    {
+        // create new bubble
+    }
+
+    public void LineEnd()
+    {
+        
+    }
+
+    public void SwitchSides(string side)
+    {
+        // who is typing right now?
+    }
+}
