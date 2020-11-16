64344e975aeba64978e1d7bfef2bb11f936f1f27
Raphael <>
2020-11-16 19:50:46 +0100

    attach input handler script to dialogue
    to enable spacebar continuing

---
+++ Assets/Scripts/General/InputHandlerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class InputHandlerBehaviour : MonoBehaviour
+{
+    public DialogueRunner dialogueRunner;
+    public DialogueUI dialogueUI;
+    // Handles Keyboard and Mouse Input
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) dialogueUI.MarkLineComplete();
+    }
+}
