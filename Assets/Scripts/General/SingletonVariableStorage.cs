2f7921cdb57ec8c1c55017db56061f755d587b4f
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2020-11-19 17:42:35 +0100

    remove redundant scripts

--- Assets/Scripts/General/SingletonVariableStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Yarn.Unity;
-
-public class SingletonVariableStorage : SingletonTemplate<InMemoryVariableStorage>
-{
-
-}
