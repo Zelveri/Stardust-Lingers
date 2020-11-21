7a2d4c3ac4ff8d055046d04933046649db1ec30e
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2020-11-21 23:19:13 +0100

    handler script for light and dark theme

---
+++ Assets/Scripts/Menu/ThemePrefHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemePrefHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("theme_color"))
+        {
+            SetLightTheme();
+        }
+    }
+
+    public static void SetLightTheme()
+    {
+        PlayerPrefs.SetString("theme_color", "Light");
+        PlayerPrefs.Save();
+    }
+
+    public static void SetDarkTheme()
+    {
+        PlayerPrefs.SetString("theme_color", "Dark");
+        PlayerPrefs.Save();
+    }
+}
