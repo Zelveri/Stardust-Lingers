d85fdb11adb092646d8a1c4fe9865d8728e92807
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2020-11-26 18:44:43 +0100

    remove useles code

--- Assets/Scripts/General/SceneController.cs
+++ Assets/Scripts/General/SceneController.cs
@@ -38,17 +38,6 @@ public class SceneController : MonoBehaviour
         SceneIsLoading = false;
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
