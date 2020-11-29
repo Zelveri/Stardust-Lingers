8249fdc21d7e343de09008ae436b94b8c91151e2
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2020-11-29 23:49:09 +0100

    new button script for title screen

---
+++ Assets/Scripts/MainMenu/ButtonBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    public void LoadScene(string scene)
+    {
+        GameManager.sceneController.LoadScene(scene);
+    }
+    public void StartStory()
+    {
+        GameManager.sceneController.StartStory();
+    }
+
+    public void ToggleMenu()
+    {
+        GameManager.sceneController.ToggleMenu();
+    }
+
+    public void ExitGame()
+    {
+        GameManager.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        GameManager.sceneController.ToMainMenu();
+    }
+}
