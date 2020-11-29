7446ecbbdc99767d499e0e06f75439a81407cc55
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2020-11-29 14:07:54 +0100

    fix the log

--- Assets/Scripts/Log/LogUpdate.cs
+++ Assets/Scripts/Log/LogUpdate.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class LogUpdate : MonoBehaviour
 {
     TMPro.TextMeshProUGUI textMesh;
+    Scrollbar scrollbar;
 
     public UnityEvent OnLoad;
     public UnityEvent OnClose;
     private void Awake()
     {
         textMesh = gameObject.transform.Find("TextContainer").gameObject.transform.Find("Text").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+        scrollbar = FindObjectOfType<Scrollbar>();
     }
     // Start is called before the first frame update
     void Start()
     {
         textMesh.text = string.Join("\n\n", DataController.GetLines());
+        scrollbar.value = 0;
         OnLoad.Invoke();
         gameObject.SetActive(true);
     }
